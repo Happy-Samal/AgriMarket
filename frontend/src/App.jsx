0a824321e91994e02584b41479b784055322fd74
Happy Samal <rudrasamal007@gmail.com>
2025-01-12 11:35:49 +0530

    remove cookiesBanner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import Router from './routes/Router';
-import CookieBanner from './components/CookieBanner';
 
 function App() {
   return (
     <div className=" bg-gradient-to-r from-[#16520f] to-[#7efb71] font-sans">
       <RouterProvider router={Router} />
-      <CookieBanner/>  {/* This is where the cookie banner is rendered  */}
     </div>
   );
 }
